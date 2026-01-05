913708bbf03c6d185ae76a338da30076c40c5f06
Rohde <Jesper.Rohde@siemensgamesa.com>
2026-01-05 04:21:05 +0100

    Update pizza app

--- push.bat
+++ push.bat
@@ -1,16 +1,12 @@
 @echo off
 echo ============================
-echo Initialiserer Git repo
+echo GitHub push starter...
 echo ============================
 
-git init
-git branch -M main
 git add .
-git commit -m "Initial commit - pizza app"
-
-git remote add origin https://vm-bingo.github.io/PIZZA/.git
-git push -u origin main
+git commit -m "Update pizza app"
+git push
 
 echo.
-echo ✅ Repo initialiseret og pushed!
+echo ✅ Push færdig!
 pause
